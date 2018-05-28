ec41b45d55d1f17cdd2e8eb29c18e8bff7ff7222
wangyifannn <2834566098@qq.com>
2018-05-28 14:38:37 +0800

    all

--- assets/js/myslideup.js
+++ assets/js/myslideup.js
@@ -1,30 +1,41 @@
-// JavaScript Document
-(function($) {
-    $.fn.extend({
-        "slideUp": function(value, myheight) {
-            // console.log(value);
-            var docthis = this;
-            //默认参数
-            value = $.extend({
-                "li_h": myheight,
-                "time": 2000,
-                "movetime": 1800
-            }, value)
+// // JavaScript Document
+// (function($) {
+//     $.fn.extend({
+//         "slideUp": function(value, myheight) {
+//             // console.log(value);
+//             var docthis = this;
+//             //默认参数
+//             //         value = $.extend({
+//             //             "li_h": myheight,
+//             //             "time": 2000,
+//             //             "movetime": 1800
+//             //         }, value)
 
-            //向上滑动动画
-            function autoani() {
-                $(".line li:first").animate({ "margin-top": -4 - value.li_h }, value.movetime, function() {
-                    $(this).css("margin-top", 0).appendTo(".line");
-                })
-            }
-            //自动间隔时间向上滑动
-            var anifun = setInterval(autoani, value.time);
-            //悬停时停止滑动，离开时继续执行
-            $(docthis).children("li").hover(function() {
-                clearInterval(anifun); //清除自动滑动动画
-            }, function() {
-                anifun = setInterval(autoani, value.time); //继续执行动画
-            })
-        }
+//             //         //向上滑动动画
+//             //         function autoani() {
+//             //             $(".line li:first").animate({ "margin-top": -4 - value.li_h }, value.movetime, function() {
+//             //                 $(this).css("margin-top", 0).appendTo(".line");
+//             //             })
+//             //         }
+//             //         //自动间隔时间向上滑动
+//             //         var anifun = setInterval(autoani, value.time);
+//             //         //悬停时停止滑动，离开时继续执行
+//             //         // $(docthis).children("li").hover(function() {
+//             //         //     clearInterval(anifun); //清除自动滑动动画
+//             //         // }, function() {
+//             //         //     anifun = setInterval(autoani, value.time); //继续执行动画
+//             //         // })
+//         }
+//     })
+
+
+// })(jQuery)
+
+function autoScroll() {
+    $(".line").animate({
+        marginTop: "-39px"
+    }, 1000, function() {
+        $(this).css({ marginTop: "0px" }).find("li:first").appendTo(this);
     })
-})(jQuery)
+}
+// var anifun = setInterval('autoScroll()', 3000);
